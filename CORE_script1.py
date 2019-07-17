f44662db4d2be4bf80354a39ea269d9ecf9a7473
Jazmin Idhali Paz <37357394+javanikeed@users.noreply.github.com>
2019-07-16 21:56:10 -0600

    Add files via upload

--- CORE_script1.py
+++ CORE_script1.py
@@ -1,22 +1,34 @@
+#!/usr/bin/env python
+
+"""CORE_script1.py: Inserts custom router configuration command to any .imn file in order to automate package capture then later safe to filesystem on its own."""
+
+__author__  = "Jazmin I. Paz"
+__version__ = "1.0.1"
+__maintainer__ = "Jazmin I. Paz"
+__email__ = "jipaz@miners.utep.edu"
+__status__ = "In Progress"
+
+
+
 import sys
 
-filename = sys.argv[1]
+file = open(sys.argv[1], "r")
+modif = open("ARL_demo.imn", "w")
+node_num=1
+_command = "    custom-config{\n\tcustom-config-id service: UserDefined\n\tcustom-command UserDefined\n\tconfig{\n\tcmdup=('tcpdump -i eth0 -n -w /tmp/n"
+command_ = ".pcap',)\n\t}\n }\n"
+#prevLine = " "
+prevLine = False;
 
-file = open(filename, "r")
-appended = open("ARL_demo.txt", "w")
-command = "    custom-config{\n\tcustom-config-id service: UserDefined\n\tcustom-command UserDefined\n\tconfig{\n\tcmdup=('tcpdump -i eth0 -n -w /tmp/n1.pcap',)\n\t}\n }\n"
-i =0
+
 for line in file:
-    appended.write(line)
-    print(i)
     if "interface-peer" in line:
-        i+=1
-        print(i)
-        break
+        prevLine = True
+    if line.startswith("}") and prevLine:
+        modif.write(_command+str(node_num)+command_)
+        node_num+=1
+        prevLine = False
     else:
-        appended.write(command)
-    
-    
-file.close()
-        
+        modif.write(line)
+file.close() 
 
