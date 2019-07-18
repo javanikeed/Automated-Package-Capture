82d8d7d9c43b5c32b8e79008024f88e366a0d0ad
Jazmin Idhali Paz <37357394+javanikeed@users.noreply.github.com>
2019-07-18 11:37:35 -0700

    Add files via upload

--- CORE_script1.py
+++ CORE_script1.py
@@ -3,7 +3,7 @@
 """CORE_script1.py: Inserts custom router configuration command to any .imn file in order to automate package capture then later safe to filesystem on its own."""
 
 __author__  = "Jazmin I. Paz"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __maintainer__ = "Jazmin I. Paz"
 __email__ = "jipaz@miners.utep.edu"
 __status__ = "In Progress"
@@ -13,7 +13,8 @@ __status__ = "In Progress"
 import sys
 
 file = open(sys.argv[1], "r")
-modif = open("ARL_demo.imn", "w")
+#modif = open("ARL_demo.imn", "w")
+modif = open(sys.argv[2], "w")
 node_num=0
 _command = "    custom-config {\n\tcustom-config-id service:UserDefined\n\n\tcustom-command UserDefined\n\tconfig {\n\tcmdup=('tcpdump -i eth0 -n -w /tmp/n"
 command_ = ".pcap', )\n\t}\n    }\n}\n"
